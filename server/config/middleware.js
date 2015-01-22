8573fe17c637522a66728e75e50330df439ed91d
David Ernst <git@dsernst.com>
2015-01-22 14:38:02 -0800

    Copied in basic server config from shortly-angular

---
+++ server/config/middleware.js
@@ -0,0 +1,26 @@
+var morgan      = require('morgan'), // used for logging incoming request
+    bodyParser  = require('body-parser'),
+    helpers     = require('./helpers.js'); // our custom middleware
+
+
+module.exports = function (app, express) {
+  // Express 4 allows us to use multiple routers with their own configurations
+  var userRouter = express.Router();
+  var linkRouter = express.Router();
+
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(express.static(__dirname + '/../../client'));
+
+
+  app.use('/api/users', userRouter); // use user router for all user request
+
+  app.use('/api/links', linkRouter); // use link router for link request
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
+
+  // inject our routers into their respective route files
+  require('../users/userRoutes.js')(userRouter);
+  require('../links/linkRoutes.js')(linkRouter);
+};
