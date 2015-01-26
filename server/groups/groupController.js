a53876c2d274e6adcfa3270302b4eeaa4db75408
David Ernst <git@dsernst.com>
2015-01-26 11:34:39 -0800

    Added groups.browse function, switched promises to use .success instead of .then

--- server/groups/groupController.js
+++ server/groups/groupController.js
@@ -3,31 +3,43 @@ var Group = require('./groupModel.js');
 module.exports = {
   findGroup: function (req, res, next, groupName) {
     Groups.findOne({where: {name: groupName}})
-    .then(function (group) {
+    .success(function (group) {
       if (!group) {
         console.log('user is searching for "' + groupName + '", but not in database');
       } else {
         req.groupId = group.id;
         next();
       }
-    })
+    });
   },
 
   create: function (req, res) {
-    Groups.sync().then(function () {
+    Groups.sync().success(function () {
       var group = Groups.build(req.body);
-      group.save();
+      group.save()
+      .success(function (result) {
+        res.end('Success:', result);
+      })
     });
   },
 
   browse: function (req, res) {
-
+    Groups.find()
+    .success(function (groups) {
+      res.end(groups);
+    });
   },
 
   join: function (req, res) {
     Users.findOne({where: {username: req.body.username}})
-    .then(function (user) {
-      user.addGroups(req.groupId);
+    .success(function (user) {
+      user.addGroups(req.groupId)
+      .success(function (result) {
+        res.end('Success:', result);
+      })
+      .error(function (err) {
+        console.log(err);
+      });
     });
   },
 
