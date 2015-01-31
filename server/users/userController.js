d8e00bdb62e84932d9ea0571614cfd33fdea138b
Michael DeLucco <mdelucco@gmail.com>
2015-01-31 10:54:57 -0800

    removed q as a dependecy

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,32 +1,32 @@
 var User = require('./userModel.js'),
-    Q    = require('q'),
+    // Q    = require('q'),
     jwt  = require('jwt-simple');
 
 module.exports = {
   signin: function (req, res, next) {
-    var username = req.body.username,
-        password = req.body.password;
+    // var username = req.body.username,
+    //     password = req.body.password;
 
-    var findUser = Q.nbind(User.findOne, User);
-    findUser({username: username})
-      .then(function (user) {
-        if (!user) {
-          next(new Error('User does not exist'));
-        } else {
-          return user.comparePasswords(password)
-            .then(function(foundUser) {
-              if (foundUser) {
-                var token = jwt.encode(user, 'secret');
-                res.json({token: token});
-              } else {
-                return next(new Error('No user'));
-              }
-            });
-        }
-      })
-      .fail(function (error) {
-        next(error);
-      });
+    // var findUser = Q.nbind(User.findOne, User);
+    // findUser({username: username})
+    //   .then(function (user) {
+    //     if (!user) {
+    //       next(new Error('User does not exist'));
+    //     } else {
+    //       return user.comparePasswords(password)
+    //         .then(function(foundUser) {
+    //           if (foundUser) {
+    //             var token = jwt.encode(user, 'secret');
+    //             res.json({token: token});
+    //           } else {
+    //             return next(new Error('No user'));
+    //           }
+    //         });
+    //     }
+    //   })
+    //   .fail(function (error) {
+    //     next(error);
+    //   });
   },
 
   signup: function (req, res, next) {
@@ -58,23 +58,23 @@ module.exports = {
     // grab the token in the header is any
     // then decode the token, which we end up being the user object
     // check to see if that user exists in the database
-    var token = req.headers['x-access-token'];
-    if (!token) {
-      next(new Error('No token'));
-    } else {
-      var user = jwt.decode(token, 'secret');
-      var findUser = Q.nbind(User.findOne, User);
-      findUser({username: user.username})
-        .then(function (foundUser) {
-          if (foundUser) {
-            res.send(200);
-          } else {
-            res.send(401);
-          }
-        })
-        .fail(function (error) {
-          next(error);
-        });
-    }
+    // var token = req.headers['x-access-token'];
+    // if (!token) {
+    //   next(new Error('No token'));
+    // } else {
+    //   var user = jwt.decode(token, 'secret');
+    //   var findUser = Q.nbind(User.findOne, User);
+    //   findUser({username: user.username})
+    //     .then(function (foundUser) {
+    //       if (foundUser) {
+    //         res.send(200);
+    //       } else {
+    //         res.send(401);
+    //       }
+    //     })
+    //     .fail(function (error) {
+    //       next(error);
+    //     });
+    // }
   }
 };
