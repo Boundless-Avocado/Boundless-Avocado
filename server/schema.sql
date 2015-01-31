f8f0c3525b2f18da3c04354d58f4a96f1f0c7caa
David Ernst <git@dsernst.com>
2015-01-31 10:40:13 -0800

    Fixed typo to grant guacman permissions on the correct db

--- server/schema.sql
+++ server/schema.sql
@@ -2,6 +2,6 @@ CREATE DATABASE avocado;
 
 # Create matching mysql user for development:
 CREATE USER 'guacman'@'localhost' IDENTIFIED BY 'Boundless1!';
-GRANT ALL ON avocados.* TO 'guacman'@'localhost';
+GRANT ALL ON avocado.* TO 'guacman'@'localhost';
 
 # TODO: tighten mysql user permissions to necessary only
