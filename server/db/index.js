0927337b18f8c4aab16dd5a614d0230a8a2c04fb
David Ernst <git@dsernst.com>
2015-01-31 10:27:25 -0800

    Switched mysql user to 'guacman'

--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,5 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('avocados', 'avocados', 'Boundless1!', {dialect: 'mysql'});
-
-// // TESTS:
-
-// exports.groupBuilder({name: 'frisbee2'});
-// exports.groupBuilder({name: 'frisbee3'});
-// exports.groupBuilder({name: 'frisbee4'});
-// exports.groupBuilder({name: 'frisbee5'});
-
-// exports.userBuilder({username: 'mikey'});
-// exports.userBuilder({username: 'michael'});
-// exports.userBuilder({username: 'michelangelo'});
-
-// exports.addToGroup('frisbee4','michael');
+var db = new Sequelize('avocados', 'guacman', 'Boundless1!', {dialect: 'mysql'});
 
 module.exports = db;
-
-//avocadoAy4XOeCXP
