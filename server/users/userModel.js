7bf11b0c0f55b9f300d1b1db892dc86ed4daf3ce
David Ernst <git@dsernst.com>
2015-01-31 17:22:01 -0800

    Removed option for null groupnames or usernames

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -2,7 +2,7 @@ var db = require('../db/index.js');
 var Sequelize = require('sequelize');
 
 var User = db.define('Users', {
-  username: {type: Sequelize.STRING, unique: true},
+  username: {type: Sequelize.STRING, unique: true, allowNull: false},
   email: Sequelize.STRING,
   phone: Sequelize.BIGINT
 });
