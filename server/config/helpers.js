987ece0573e8290c6259b97850a43ebe274cf348
David Ernst <git@dsernst.com>
2015-02-02 11:18:53 -0800

    Removed unnused decode() jwt helper function

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,5 +1,3 @@
-var jwt  = require('jwt-simple');
-
 module.exports = {
   errorLogger: function (error, req, res, next) {
     // log the error then send it to the next middleware in
@@ -12,25 +10,5 @@ module.exports = {
     // send error message to client
     // message for gracefull error handling on app
     res.status(500).send({error: error.message});
-  },
-
-  decode: function (req, res, next) {
-    var token = req.headers['x-access-token'];
-    var user;
-
-    if (!token) {
-      return res.send(403); // send forbidden if a token is not provided
-    }
-
-    try {
-      // decode token and attach user to the request
-      // for use inside our controllers
-      user = jwt.decode(token, 'secret');
-      req.user = user;
-      next();
-    } catch(error) {
-      return next(error);
-    }
-
   }
 };
